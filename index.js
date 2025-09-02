b694acd9363a77a1b0b5641305fbff30266a574e
git-felipeUrtubia <felipe.urtubia7@gmail.com>
2025-09-02 19:15:29 -0400

    first commit

--- index.js
+++ index.js
@@ -5,23 +5,41 @@ const btn_submit = document.getElementById("btn-submit");
 const form_register_style = document.querySelector(".form-register-style");
 const form_box_check = document.querySelector(".form-box-check");
 
+const inputsForm1 = form_register_style.querySelectorAll("input");
+const message = form_register_style.querySelectorAll("p");
+const content_message = document.querySelector('.content-message');
+
+let element = [];
+btn_next.disabled = false;
 btn_next.addEventListener('click', () => {
+    
+    let vacio = false;
 
-    form_register_style.style.transform = 'translateY(-120vh)';
-    form_box_check.style.transform = 'translateY(0vh)';
+    inputsForm1.forEach(input => {
+        if (input.value.trim() === "" || input.value == null) {
+            vacio = true;
+            // alert(`El campo "${input.name}" está vacío`);
+            message.textContent = `Verificar ${input.name}`;
+        }
+    });
 
-    // const nombre_completo = document.getElementById('input-full-name');
-    // const email = document.getElementById('input-email');
-    // const passd = document.getElementById('input-passd');
-    // const conf_passd = document.getElementById('input-conf-passd');
-    // const phone = document.getElementById('input-phone');
+    if (vacio) {
+        btn_next.disabled = true;
+    }else {
+        btn_next.disabled = false;
+        form_register_style.style.transform = 'translateY(-120vh)';
+        form_box_check.style.transform = 'translateY(0vh)';
+    }
+
 });
 
 const form1 = document.getElementById('form1');
 const form2 = document.getElementById('form2');
 
+
 btn_submit.addEventListener('click', (event) => {
     event.preventDefault();
+
     const obj1 = Object.fromEntries(new FormData(form1));
     const dataCheck = new FormData(form2);
 
