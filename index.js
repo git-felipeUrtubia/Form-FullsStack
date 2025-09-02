c5fc7963ef3aa29364b57f1bc1357c150edf74d3
git-felipeUrtubia <felipe.urtubia7@gmail.com>
2025-09-02 10:46:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const btn_next = document.getElementById("btn-next");
+const form_register = document.querySelector(".form-register");
+const form_box_check = document.querySelector(".form-box-check");
+
+btn_next.addEventListener('click', () => {
+
+    form_register.style.transform = 'translateY(-120vh)';
+    form_box_check.style.transform = 'translateY(0vh)';
+
+
+});
+
+
+
+
+
+
+
+
+
