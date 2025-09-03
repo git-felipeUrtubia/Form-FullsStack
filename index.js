6cd9a4f00a4a218b50539d279b7f375dcc8e17ee
git-felipeUrtubia <felipe.urtubia7@gmail.com>
2025-09-03 12:45:44 -0400

    first commit

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 
 const btn_next = document.getElementById("btn-next");
 const btn_submit = document.getElementById("btn-submit");
-
 const form_register_style = document.querySelector(".form-register-style");
 const form_box_check = document.querySelector(".form-box-check");
-
 const inputsForm1 = form_register_style.querySelectorAll("input");
-const message = form_register_style.querySelectorAll("p");
+const input_email = document.getElementById('input-email'); 
+const input_passd = document.getElementById('input-passd');
+const input_conf_passd = document.getElementById('input-conf-passd');
 const content_message = document.querySelector('.content-message');
+const message = form_register_style.querySelector("p");
 
-let element = [];
-btn_next.disabled = false;
 btn_next.addEventListener('click', () => {
     
-    let vacio = false;
 
-    inputsForm1.forEach(input => {
-        if (input.value.trim() === "" || input.value == null) {
-            vacio = true;
-            // alert(`El campo "${input.name}" está vacío`);
-            message.textContent = `Verificar ${input.name}`;
-        }
-    });
+    // verificar si campos vacios para avanzar
+    let vacio = Array.from(inputsForm1).some(input => 
+    input.value.trim() == "" || 
+    input.value.trim() == null);
+
+    // verificar formato de email
+    let email_check = input_email.value.trim();
+    const format_email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/; 
+
+    // verificar contraseña
+    let passd_1 = input_passd.value.trim();
+    let passd_2 = input_conf_passd.value.trim();
 
     if (vacio) {
-        btn_next.disabled = true;
+
+        content_message.style.display = "flex";
+        message.textContent = "* inputs vacios";
+
+    }else if(!format_email.test(email_check)) {
+
+        content_message.style.display = "flex";
+        message.innerHTML = "";
+        message.innerHTML = 
+        '* Asegúrate de escribir un correo valido. Ejemplo: <a href="#" >nombre@ejemplo.com</a>';
+
+    }else if(passd_1.length < 8) {
+        content_message.style.display = "flex";
+        message.textContent = "* La contraseña debe tener minimo 8 caracteres.";
+    }else if(passd_1 !== passd_2) {
+        content_message.style.display = "flex";
+        message.textContent = "* Las contraseñas no coinciden. Por favor, inténtalo de nuevo.";
     }else {
-        btn_next.disabled = false;
+
         form_register_style.style.transform = 'translateY(-120vh)';
         form_box_check.style.transform = 'translateY(0vh)';
+    
     }
 
 });
@@ -44,7 +64,8 @@ btn_submit.addEventListener('click', (event) => {
     const dataCheck = new FormData(form2);
 
     let datos = {
-        full_name: obj1.full_name,
+        first_name: obj1.first_name,
+        last_name: obj1.last_name,
         email: obj1.email,
         passd: obj1.passd,
         conf_passd: obj1.conf_passd,
@@ -52,9 +73,9 @@ btn_submit.addEventListener('click', (event) => {
         generos: dataCheck.getAll("genero")
     };
 
-    // for(let key in datos) {
-    //     console.log(key, datos[key])
-    // }
+    for(let key in datos) {
+        console.log(key, datos[key])
+    }
 
 })
 
